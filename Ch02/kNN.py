16321918797fdecf8cd3b96be29b77654a1da21a
ylhao <765422195@qq.com>
2018-01-19 10:26:37 +0800

    ch02

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -63,14 +63,16 @@ def autoNorm(dataSet):
 
 def datingClassTest():
     hoRatio = 0.50      #hold out 10%
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
+    datingDataMat,datingLabels = file2matrix('datingTestSet.txt')       #load data setfrom file
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if i < 10:
+            print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
         if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "..."
     print "the total error rate is: %f" % (errorCount/float(numTestVecs))
     print errorCount
