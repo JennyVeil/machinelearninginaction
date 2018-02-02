8c2af2ec4dfbae4ad56310ecc21c0e82d673d4b8
ylhao <765422195@qq.com>
2018-02-02 15:08:19 +0800

    ch07 代码注释

--- Ch07/adaboost.py
+++ Ch07/adaboost.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 '''
 Created on Nov 28, 2010
 Adaboost is short for Adaptive Boosting
@@ -6,6 +8,7 @@ Adaboost is short for Adaptive Boosting
 from numpy import *
 
 def loadSimpData():
+    # 加载数据，非常简单的数据
     datMat = matrix([[ 1. ,  2.1],
         [ 2. ,  1.1],
         [ 1.3,  1. ],
@@ -28,6 +31,13 @@ def loadDataSet(fileName):      #general function to parse tab -delimited floats
     return dataMat,labelMat
 
 def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):#just classify the data
+    """
+    单层决策树
+    dataMatrix: 数据集
+    dimen: 哪个特征
+    threshaVal: 阙值
+    threshIneq: 大于阙值还是小于阙值
+    """
     retArray = ones((shape(dataMatrix)[0],1))
     if threshIneq == 'lt':
         retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
@@ -36,22 +46,37 @@ def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):#just classify the data
     return retArray
     
 
+# 寻找最佳单层决策树
 def buildStump(dataArr,classLabels,D):
     dataMatrix = mat(dataArr); labelMat = mat(classLabels).T
+    # 这里 m 就是样本个数，n 就是每个样本的特征数
     m,n = shape(dataMatrix)
-    numSteps = 10.0; bestStump = {}; bestClasEst = mat(zeros((m,1)))
+    numSteps = 10.0
+    # 存储给定权重向量D时所得到的最佳单层决策树的相关信息
+    bestStump = {}
+    bestClasEst = mat(zeros((m,1)))
     minError = inf #init error sum, to +infinity
+    # 遍历每个特征
     for i in range(n):#loop over all dimensions
-        rangeMin = dataMatrix[:,i].min(); rangeMax = dataMatrix[:,i].max();
+        # 找到这个特征对应的值的最小值和最大值
+        rangeMin = dataMatrix[:,i].min();
+        rangeMax = dataMatrix[:,i].max();
+        # 根据定义的步数，计算步长
         stepSize = (rangeMax-rangeMin)/numSteps
         for j in range(-1,int(numSteps)+1):#loop over all range in current dimension
             for inequal in ['lt', 'gt']: #go over less than and greater than
+                # 确定阙值
                 threshVal = (rangeMin + float(j) * stepSize)
+                # 调用 stumpClassify() 得到分类列表
                 predictedVals = stumpClassify(dataMatrix,i,threshVal,inequal)#call stump classify with i, j, lessThan
+                # 统计哪个样本分类错误
                 errArr = mat(ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
+                # 计算加权错误率（在这里用到了每个样本的权重）
+                # 如果某个样本的权重高，这个样本在这次分类中分类错误，那么它带来的错误率就高
+                # 通俗的说就是把重要的样本分错了（这种情况是应该避免的）
                 weightedError = D.T*errArr  #calc total error multiplied by D
-                #print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
+                # print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
                 if weightedError < minError:
                     minError = weightedError
                     bestClasEst = predictedVals.copy()
@@ -62,29 +87,39 @@ def buildStump(dataArr,classLabels,D):
 
 
 def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    # 用来存放所有的弱分类器
     weakClassArr = []
+    # 样例个数为 m
     m = shape(dataArr)[0]
+    # 初始化每个样例的权重
     D = mat(ones((m,1))/m)   #init D to all equal
     aggClassEst = mat(zeros((m,1)))
+    # 最多训练 numIt 个弱分类器
     for i in range(numIt):
+        # 寻找最佳单层决策树
         bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
-        #print "D:",D.T
+        # print "D:",D.T
+        # 计算该分类器的权重
         alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
         bestStump['alpha'] = alpha  
         weakClassArr.append(bestStump)                  #store Stump Params in Array
-        #print "classEst: ",classEst.T
+        # print "classEst: ",classEst.T
+        # 以下三行合起来就是更新每个样例的权重
         expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
         D = multiply(D,exp(expon))                              #Calc New D for next iteration
         D = D/D.sum()
         #calc training error of all classifiers, if this is 0 quit for loop early (use break)
+        # aggClassEst 是各个弱分类器的加权和
         aggClassEst += alpha*classEst
-        #print "aggClassEst: ",aggClassEst.T
+        # print "aggClassEst: ",aggClassEst.T
         aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
         errorRate = aggErrors.sum()/m
         print "total error: ",errorRate
         if errorRate == 0.0: break
+    # 返回所有的弱分类器
     return weakClassArr
 
+# 使用所有的弱分类器进行分类
 def adaClassify(datToClass,classifierArr):
     dataMatrix = mat(datToClass)#do stuff similar to last aggClassEst in adaBoostTrainDS
     m = shape(dataMatrix)[0]
